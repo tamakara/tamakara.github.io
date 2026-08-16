4780b6de403bd103ccadf5edd2397b473280c3c4
魂辛カラ <tamakara@qq.com>
2026-08-17 06:20:14 +0800

    优化

--- src/config.ts
+++ src/config.ts
@@ -56,7 +56,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // 相对于 /src 目录；如果以 '/' 开头，则相对于 /public 目录
 	name: "魂辛カラ",
-	bio: "INTJ\n喜欢计算机、数学、二次元、音乐和可爱的东西。",
+	bio: "喜欢计算机、数学、二次元、音乐和可爱的东西。",
 	links: [
 		{
 			name: "GitHub",
