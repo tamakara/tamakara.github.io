24f0c22f23fbd820857146461b86ef1852d62e41
魂辛カラ <tamakara@qq.com>
2026-08-17 04:32:24 +0800

    修改配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://tamakara.github.io",
+	site: "https://tamakara.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
